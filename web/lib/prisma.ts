2e9a6556d78cf2ce196d05c12e313e6845e29e68
tonkar595 <tonkar595@gmail.com>
2026-03-08 13:18:14 +0700

    fix prisma

--- web/lib/prisma.ts
+++ web/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../app/generated/prisma/client'
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 export const prisma = globalForPrisma.prisma || new PrismaClient()
